e3a4ce092c29daa5298cd28a6817db1f1d94cb34
hosozso <70272121+hosozso@users.noreply.github.com>
2025-09-28 20:36:57 +0200

    chore: initial Spring Boot project setup

---
+++ src/main/java/hu/unideb/cartshare/CartshareApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.cartshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/cartshare/CartshareApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.cartshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
