3be77e00d7d42de8cb93bd005d765ed96ecf831b
hosozso <70272121+hosozso@users.noreply.github.com>
2025-10-05 20:34:28 +0200

    feat: add validation for the existing DTOs with ControllerAdvice

--- src/main/java/hu/unideb/cartshare/dto/ListDto.java
+++ src/main/java/hu/unideb/cartshare/dto/ListDto.java
@@ -1,5 +1,7 @@
 package hu.unideb.cartshare.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,10 +12,15 @@ import java.util.List;
 @Setter
 @Builder
 public class ListDto {
+    @JsonProperty(
+            access = JsonProperty.Access.READ_ONLY
+    )
     private String id;
 
+    @NotEmpty(message = "Kitöltése kötelező")
     private String name;
 
+    @NotEmpty(message = "Kitöltése kötelező")
     private List<ListItemDto> items;
 
 }
--- src/main/java/hu/unideb/cartshare/dto/ListItemDto.java
+++ src/main/java/hu/unideb/cartshare/dto/ListItemDto.java
@@ -1,5 +1,8 @@
 package hu.unideb.cartshare.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,9 +11,14 @@ import lombok.Setter;
 @Setter
 @Builder
 public class ListItemDto {
+    @JsonProperty(
+            access = JsonProperty.Access.READ_ONLY
+    )
     private String id;
 
+    @NotEmpty(message = "Kitöltése kötelező")
     private String name;
 
+    @NotNull(message = "Kitöltése kötelező")
     private Boolean isChecked;
 }
