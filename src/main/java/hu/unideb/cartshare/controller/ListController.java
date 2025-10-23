38436da9de2e61c4f30adf5e5d7fdecbc89b5fbb
hb3nce04 <60313402+hb3nce04@users.noreply.github.com>
2025-10-23 15:12:05 +0200

    fix: wrong merging

--- src/main/java/hu/unideb/cartshare/controller/ListController.java
+++ src/main/java/hu/unideb/cartshare/controller/ListController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
