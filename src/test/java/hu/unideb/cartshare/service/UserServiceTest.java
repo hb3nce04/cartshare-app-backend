cecaad964e01a49d7e424debc4ed749df4a6e13a
hosozso <70272121+hosozso@users.noreply.github.com>
2025-11-01 20:53:21 +0100

    test: add UserServiceTest.java

---
+++ src/test/java/hu/unideb/cartshare/service/UserServiceTest.java
@@ -0,0 +1,25 @@
+package hu.unideb.cartshare.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createLocalUser() {
+    }
+
+    @Test
+    void findOrCreateGoogleUser() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
